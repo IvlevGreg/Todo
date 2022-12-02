779ddaba4c4535906365d62de7d4cbdb7f2d4944
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2022-12-02 17:39:47 +0300

    delete initial value &  reorganized files
    delete initial value

--- 03_string-boolean-conditionals/ex1.js
+++
@@ -1,7 +0,0 @@
-let password = 'qaz'
-
-if (password.length>=4 && (password.includes('-') || password.includes('_')) ) {
-  console.log ('Пароль надёжный')
-} else {
-  console.log ('Пароль недостаточно надёжный')
-}
